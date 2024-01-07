863a3aa3be412f83cce513f65b246e138efd0da2
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-01-07 11:41:27 -0800

    Add files via upload

---
+++ command.js
@@ -0,0 +1,6 @@
+chrome.commands.onCommand.addListener((command) => {
+    if(command === "inject-script") {
+        //console.log(`Command "${command}" triggered`);
+        //chrome.tabs.create({ url: '' });
+    }
+});
